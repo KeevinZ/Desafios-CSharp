0df30f724115c4310045124b7e1269d5cf0e2299
Kevin Henrique Davi <kevin1080p.mdm@gmail.com>
2025-10-10 13:12:48 -0300

    1º desafio - Estrutura de repetição ( for )

---
+++ 1a10ComEstrRepeticao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int soma = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            soma += i;
+        }
+
+        Console.WriteLine($"A soma dos números de 1 a 10 é: {soma}");
+    }
+}
