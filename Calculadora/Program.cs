4329629900ee59bddfae6c27c86fce197725de07
Kevin Henrique Davi <kevin1080p.mdm@gmail.com>
2025-10-10 13:13:53 -0300

    2º desafio - Calculadora Simples com switch

---
+++ Calculadora/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Escolha a operação (+, -, *, /): ");
+        char operacao = char.Parse(Console.ReadLine());
+
+        double resultado = 0;
+        bool operacaoValida = true;
+
+        switch (operacao)
+        {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    resultado = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Erro: divisão por zero!");
+                    operacaoValida = false;
+                }
+                break;
+            default:
+                Console.WriteLine("Operação inválida!");
+                operacaoValida = false;
+                break;
+        }
+
+        if (operacaoValida)
+            Console.WriteLine($"\nResultado: {resultado}");
+    }
+}
+
+
